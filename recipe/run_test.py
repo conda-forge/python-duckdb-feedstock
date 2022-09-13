6e023a28a8ac46772e8078e151be2fd41c3e93a0
Gil Forsyth <gil@forsyth.dev>
2022-09-13 15:50:08 -0400

    fix: use the right hash and add extension install test

---
+++ recipe/run_test.py
@@ -0,0 +1,11 @@
+import duckdb
+
+
+def main():
+    # Test that we can resolve extensions shipped by the duckdb team
+    con = duckdb.connect()
+    con.install_extension("postgres_scanner")
+
+
+if __name__ == "__main__":
+    main()
