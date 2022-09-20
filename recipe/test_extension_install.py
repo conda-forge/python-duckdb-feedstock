059a44c37f99131c022f4e0ac779ef0c10937931
Gil Forsyth <gil@forsyth.dev>
2022-09-20 11:08:51 -0400

    test: also attempt loading extension

--- recipe/test_extension_install.py
+++ recipe/test_extension_install.py
@@ -5,6 +5,7 @@ def main():
     # Test that we can resolve extensions shipped by the duckdb team
     con = duckdb.connect()
     con.install_extension("postgres_scanner")
+    con.load_extension("postgres_scanner")
 
 
 if __name__ == "__main__":
