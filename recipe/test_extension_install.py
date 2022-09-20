96b8d1cc069be11af4ccbf45eb1ced459950030c
Gil Forsyth <gil@forsyth.dev>
2022-09-20 16:29:31 -0400

    test: try sqlite scanner extension
    postgres scanner is currently broken and this isn't supposed to be
    testing whether upstream extensions were built correctly

--- recipe/test_extension_install.py
+++ recipe/test_extension_install.py
@@ -4,8 +4,8 @@ import duckdb
 def main():
     # Test that we can resolve extensions shipped by the duckdb team
     con = duckdb.connect()
-    con.install_extension("postgres_scanner")
-    con.load_extension("postgres_scanner")
+    con.install_extension("sqlite_scanner")
+    con.load_extension("sqlite_scanner")
 
 
 if __name__ == "__main__":
