abfee3347122c95457496afc0f9bfc7ffd8ad975
Gil Forsyth <gil@forsyth.dev>
2022-09-21 13:43:15 -0400

    chore: remove load test (out of our hands)

--- recipe/test_extension_install.py
+++ recipe/test_extension_install.py
@@ -5,7 +5,6 @@ def main():
     # Test that we can resolve extensions shipped by the duckdb team
     con = duckdb.connect()
     con.install_extension("sqlite_scanner")
-    con.load_extension("sqlite_scanner")
 
 
 if __name__ == "__main__":
