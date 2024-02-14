df61dd2518ffa96a69156ba6866820fa09deb7eb
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-14 13:40:52 +0100

    Export the flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
   # avoid error 'relocation truncated to fit: R_PPC64_REL24'
-  CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g')"
-  CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g')"
+  export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g')"
+  export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g')"
 fi
 
 ${PYTHON} -m pip install tools/pythonpkg -vv
