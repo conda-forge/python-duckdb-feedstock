baef96691d84383e013233305c95fe8f292142aa
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-14 15:09:14 +0100

    Force PLT

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
   # avoid error 'relocation truncated to fit: R_PPC64_REL24'
-  export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g')"
-  export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g')"
+  export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g') -fplt"
+  export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g') -fplt"
 fi
 
 ${PYTHON} -m pip install tools/pythonpkg -vv
