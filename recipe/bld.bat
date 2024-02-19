9f93c066f1ccf01a5370974c39d761f600386b21
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-19 16:47:02 +0100

    Delete problematic file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
 @echo on
 
 %PYTHON% -m pip install tools/pythonpkg -vv
+@rem Delete this file as conda-build trips over it
+del test\sql\copy\csv\data\glob\crawl\.symbolic_link\mydir\link_to_upper_dir
