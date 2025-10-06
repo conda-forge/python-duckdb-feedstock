1132afe62ffc628fee0d678c6c87de54cfcbdfeb
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-06 12:16:23 +0200

    Build from the sdist

--- recipe/build-duckdb.sh
+++ recipe/build-duckdb.sh
@@ -12,8 +12,11 @@ if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc6
   # jemalloc extension on aarch64/ppc64le needs pthread symbols
   export CFLAGS="${CFLAGS} -pthread"
   export CXXFLAGS="${CXXFLAGS} -pthread"
+  export JEMALLOC_SYS_WITH_LG_PAGE=16
 fi
 
-export OVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-g2063dda
 
-${PYTHON} -m pip install --no-deps --no-build-isolation tools/pythonpkg -vv
+# export OVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-gb8a06e4
+export CMAKE_GENERATOR=Ninja
+
+${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
